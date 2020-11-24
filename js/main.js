409479ebf74b3d2a633309240636e2a4aab711db
nourhanElkashef <nourhan@roots.solutions>
2020-11-24 17:01:58 +0200

    edit about page , create contact page , create customer page , edit css ,make design responsive

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* setTimeout(() => {
+    let h1 = document.querySelector('#navHome');
+    let h2 = document.querySelector('#navAboutUs');
+    let h3 = document.querySelector('#navSolutions');
+    let h4 = document.querySelector('#navCustomers');
+    let h5 = document.querySelector('#navCareers');
+    let h6 = document.querySelector('#navContact');
+    console.log(h1);
+    h1.addEventListener('click', function() {
+        Location.href = '/index.html'
+    })
+    h2.addEventListener('click', function() {
+        Location.href = '/about.html'
+    })
+}, 300) */
